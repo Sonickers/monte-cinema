60775bef862db6993e946f1f8a7ea870c413633e
Sonia Janota <sonia.janota@monterail.com>
2021-06-02 13:25:48 +0200

    Fix singleton method

--- app/models/reservation_status.rb
+++ app/models/reservation_status.rb
@@ -1,7 +1,7 @@
 class ReservationStatus < ApplicationRecord
   # Create methods for retrieving all statuses directly
   ['Booked', 'Confirmed', 'Cancelled'].each do |name|
-    singleton_class.send(:define_method, name.downcase) do
+    define_singleton_method(name.downcase) do
       find_by(name: name)
     end
   end
