430748afd798db8f21fb6c4f63d56b1b7728a918
Artur Rosa <artur.rosa@monterail.com>
2022-02-18 12:18:43 +0100

    Disable sentry except for production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,6 @@
 Sentry.init do |config|
   config.dsn = 'https://39ba3df194d04d50bac838121459e870@o877093.ingest.sentry.io/5827390'
+  config.enabled_environments = %w[production]
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
